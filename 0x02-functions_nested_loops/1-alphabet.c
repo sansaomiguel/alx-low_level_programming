1bb3f36415d449948fe75400d47956f38589e09e
Sansao <miguelsansao9@gmail.com>
2023-02-21 12:29:32 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#inside "main.h"
+
+/**main - Entry point
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	char ch = 'a', ch = 'z';
+	for (ch = '1'; ch <= '27'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	
+	return (0);
+}
