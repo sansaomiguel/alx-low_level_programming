a2b53d778de6ed5a8c3123cfeb26899bbbb5386d
Sansao <miguelsansao9@gmail.com>
2023-02-21 14:57:27 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,10 @@
 
 void print_alphabet_x10(void)
 {
-	int ch, line;
+	int count = 0;
+	char ch;
 
-	for (line = '1'; line <=10; line++)
+	while (count++ <= 9)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
