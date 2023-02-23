0473f6ebd57232175aba863419de25995991dcd0
Sansao <miguelsansao9@gmail.com>
2023-02-23 06:42:13 -0800

    bug finally fixed

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
-
 int main(void)
 {
-	int a,b,c; 
+	int a, b, c;
 	int largest;
-	
+
 	a = 972;
 	b = -98;
 	c = 0;
-	
-	int largest_number(int a, int b, int c)
-		
+
+	largest = largest_number(a, b, c);
+
 	printf("%d is the largest number\n", largest);
-	
+
 	return (0);
 }
