c3d18627b346fa16c46b099a8ac9779c7bfac9e6
Sansao <miguelsansao9@gmail.com>
2023-02-24 06:03:55 -0800

    Digit Checker

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
+/**
+ * _isdigit - Function that checks for a digit
+ * @c: character that has to be checked
+ * Return: 1 if c is a digit, c if its not
+ *
+ */
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-	
+	if (c >= '48' && c <= '57')i
+
 		return (1);
-	
+
 	else
 		return (0);
-}	
+}
