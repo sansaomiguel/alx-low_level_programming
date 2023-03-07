b433182ba693149882cc93327de7f554d9cd4fc9
Sansao <miguelsansao9@gmail.com>
2023-03-07 00:54:27 +0000

    Hot Fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,4 +9,22 @@
  * or NULL, if substring is not found
  */
 
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
 
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+		while (*n != '\0' && *haystack == *n)
+		{
+			haystack++;
+			n++;
+		}
+		if (!*n)
+			return (h);
+		haystack++;
+	}
+	return ('\0');
+}
