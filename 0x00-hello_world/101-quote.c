0502876330e0ecdb34773c1d0868b3fefdc24593
Sansao Miguel <miguelsansao9@gmail.com>
2023-05-20 16:18:56 -0700

    printing a stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	strerror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
