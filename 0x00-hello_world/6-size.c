facbaf025ab80e7bb7079099917b5eb238955b04
Sansao Miguel <miguelsansao9@gmail.com>
2023-05-20 14:55:06 -0700

    prints numbers of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	puts("Size of a char: %zu byte(s)\n", sizeof(char));
+	puts("Size of an int: %zu byte(s)\n", sizeof(int));
+	puts("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	puts("Size of a long long int: %zu byte(s)\n", sizeof(long long long int));
+	puts("Size of a float: %zu byte(s)\n", sizeof(float);
+			return (0);
+}
