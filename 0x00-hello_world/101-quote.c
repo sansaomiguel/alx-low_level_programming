b7a9265d40d4d9ea77482575ab55f266072bcce1
Sansao Miguel <miguelsansao9@gmail.com>
2023-05-21 05:49:37 -0700

    Probram that prints to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  * main - Entry point
- * Return: 0
+ * Return: 1
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
