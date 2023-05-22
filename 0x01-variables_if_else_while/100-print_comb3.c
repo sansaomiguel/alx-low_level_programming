fdefa70324f4defd5c16228c61744c2fadff67ec
Sansao Miguel <miguelsansao9@gmail.com>
2023-05-22 07:21:58 -0700

    program that prints all possible different combinations of two digits separated by comma

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,21 @@
  */
 int main(void)
 {
-	int n = 0;
+	int n = 1;
 	int nn = 0;
 
-	while (n < 10)
+	while (n <= 9)
 	{
-		while (nn < 10)
+		while (nn <= 9)
 		{
 			putchar('0' + nn);
 			putchar('0' + n);
+			
+			if (n != 9 || nn != 9)
+			{	
+				putchar(',');
+				putchar(' ');
+			}
 			nn++;
 		}
 		n++;
