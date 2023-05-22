0e283bcc740fa8acbec4ed6659b0515cc18d06ca
Sansao Miguel <miguelsansao9@gmail.com>
2023-05-22 04:54:36 -0700

    program that prints numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 15)
+	{
+		if (n < 10)
+		{
+			putchar('0' + n);
+		}
+		else 
+		{
+			putchar('a' + (n - 10));
+		}
+	++n;
+	}
+	putchar('\n');
+	return (0);
+}
